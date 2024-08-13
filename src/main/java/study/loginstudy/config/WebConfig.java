9f30825665e34b40486f23a6bd71466d1cc24afe
yeonchaepark <check4732@naver.com>
2024-08-13 13:15:01 +0900

    config수정

--- src/main/java/study/loginstudy/config/WebConfig.java
+++ src/main/java/study/loginstudy/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:52827")
+//                .allowedOrigins("http://127.0.0.1:52827")
+                .allowedOrigins("http://127.0.0.1:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }

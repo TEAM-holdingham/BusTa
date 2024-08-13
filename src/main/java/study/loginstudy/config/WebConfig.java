c3e51bd3b81de12e3f5b64870f48cb8691eaa8bf
yeonchaepark <check4732@naver.com>
2024-08-13 17:38:56 +0900

    와일드카드변경

--- src/main/java/study/loginstudy/config/WebConfig.java
+++ src/main/java/study/loginstudy/config/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:8080") // 허용할 출처
+                .allowedOrigins("*") // 허용할 출처->전체로 변경
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
                 .allowCredentials(true)
                 .allowedHeaders("*")

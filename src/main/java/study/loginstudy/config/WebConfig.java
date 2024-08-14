b44b0a7060bb24e44a8f6397e7083a4b14db09e1
seungyeons2 <seungyeonsi913@gmail.com>
2024-08-14 13:00:01 +0900

    fix: WebConfig

--- src/main/java/study/loginstudy/config/WebConfig.java
+++ src/main/java/study/loginstudy/config/WebConfig.java
@@ -1,8 +1,3 @@
-package study.loginstudy.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@Configuration
 //public class WebConfig implements WebMvcConfigurer {
@@ -16,16 +11,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //    }
 //}
 
+package study.loginstudy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080") // 허용할 출처->전체로 변경
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
-                .allowCredentials(true)
+                .allowedOrigins("http://localhost:8080", "http://127.0.0.1:8080", "https://port-0-busta-lyumntwj5a7765e6.sel4.cloudtype.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .exposedHeaders("Authorization", "Link")
-                .maxAge(3600); // pre-flight 요청의 결과를 캐시하는 시간 (초 단위)
+                .allowCredentials(true);
     }
 }
